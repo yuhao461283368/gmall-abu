a7eb4c30dccafbe977c5b44e7a1fca4015aaab49
abu <1111@qq.com>
2020-04-24 20:39:18 +0800

    库存管理

--- gmall-wms/src/main/java/com/atguigu/gmall/wms/controller/WareSkuController.java
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/controller/WareSkuController.java
@@ -1,20 +1,19 @@
 package com.atguigu.gmall.wms.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.QueryCondition;
 import com.atguigu.core.bean.Resp;
+import com.atguigu.gmall.wms.entity.WareSkuEntity;
+import com.atguigu.gmall.wms.service.WareSkuService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import com.atguigu.gmall.wms.entity.WareSkuEntity;
-import com.atguigu.gmall.wms.service.WareSkuService;
+import java.util.Arrays;
+import java.util.List;
 
 
 
@@ -32,6 +31,16 @@ import com.atguigu.gmall.wms.service.WareSkuService;
 public class WareSkuController {
     @Autowired
     private WareSkuService wareSkuService;
+
+
+    @ApiOperation("根据skuId查询库存信息")
+    @GetMapping("{skuId}")
+    public Resp<List<WareSkuEntity>> queryWareSkuBySkuId(@PathVariable("skuId")Long skuId){
+
+        List<WareSkuEntity> wareSkuEntities = this.wareSkuService.list(new QueryWrapper<WareSkuEntity>().eq("sku_id", skuId));
+
+        return Resp.ok(wareSkuEntities);
+    }
 
     /**
      * 列表
